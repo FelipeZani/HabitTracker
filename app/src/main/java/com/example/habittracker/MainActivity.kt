460d69334a5acc5296fe21dc2244787c0b2462a7
Felipe <amanciofzani@gmail.com>
2024-10-10 20:29:07 +0200

    Calendar Ui added

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -6,20 +6,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.habittracker.ui.theme.HabitTrackerTheme
@@ -30,20 +39,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HabitTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), //main container of the app
-                        floatingActionButton = {
-                            FloatingActionButton(
-                                onClick = {}
-                            ){
-                                Icon(imageVector = Icons.Default.Add, contentDescription = "Open a menu to add an new habit")
-                            }
-                        }
+                ContentApp()
 
-                ) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)
-                        .padding(16.dp)
+            }
+        }
+    }
+}
+
+@Composable
+fun ContentApp(){
+    Scaffold(modifier = Modifier.fillMaxSize(), //main container of the app
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {}
+            ){
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Open a menu to add an new habit")
+            }
+        }
+
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)
+            .padding(16.dp)
+        ) {
+            HabitStreak()
+        }
+    }
+}
+
+@Composable
+fun HabitStreak() {
+    Row(){
+        LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp),
+            ){
+            items(count = 7){
+                Card(
+                    modifier = Modifier
+                        .padding(vertical = 4.dp, horizontal = 4.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    ),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .width(40.dp)
+                            .height(48.dp)
+                            .padding(4.dp)
                     ) {
-                        HabitStreak()
+                        Text(
+                            text = "day",
+                           modifier = Modifier.align(Alignment.CenterHorizontally),
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Text(
+                            text = "date",
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            style = MaterialTheme.typography.bodyMedium,
+                        )
                     }
                 }
             }
@@ -51,17 +102,9 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun HabitStreak() {
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp)){
-        items(count = 7){
-            Button(onClick = {}) { }
-        }
-    }
-}
 
 @Preview(showSystemUi = true)
 @Composable
 fun ShowContent() {
-    HabitStreak()
+    ContentApp()
 }
